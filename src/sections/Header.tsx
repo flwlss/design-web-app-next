75a4fb5b3db7fa5d34b5cf6c08e98e91f0cfd31e
Dmitry <dimaandreev1337@gmail.com>
2023-11-01 23:05:52 +0700

    fix header

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,9 +1,16 @@
+import Container from "@/components/Container";
 import Navbar from "@/components/Navbar";
 
 const Header = () => {
   return (
     <header className="header">
       <Navbar />
+      <Container>
+        <div className="header__content">
+          <h1>Современная архитектура</h1>
+          <h2>воплоти свою мечту в жизнь</h2>
+        </div>
+      </Container>
     </header>
   );
 };
